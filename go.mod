c6ca117b4d0a3a2da48a80ec81705b0f2a808330
Cyro Dubeux <xorycx@gmail.com>
2019-05-14 00:14:36 -0300

    Album service implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cyruzin/hexagony
 
 go 1.12
+
+require github.com/google/uuid v1.1.1
--- internal/app/albums/service.go
+++ internal/app/albums/service.go
@@ -1,1 +1,62 @@
 package albums
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	// ErrInvalidID is returned when the provided ID is invalid.
+	ErrInvalidID = errors.New("invalid id provided")
+
+	// ErrDuplicate is used when a album already exists.
+	ErrDuplicate = errors.New("album already exists")
+
+	// ErrInvalidPrice is returned when the provided price is invalid.
+	ErrInvalidPrice = errors.New("invalid price provided")
+
+	// ErrNotFound is returned when the album is not found.
+	ErrNotFound = errors.New("could not find the album")
+)
+
+// Service provides albums operations.
+type Service interface {
+	AddAlbum(ctx context.Context, album *Album) error
+	DeleteAlbum(ctx context.Context, uuid uuid.UUID) error
+}
+
+// Repository provides access to albums repository.
+type Repository interface {
+	Add(context.Context, *Album) error
+	Find(context.Context, uuid.UUID) (*Album, error)
+	FindAll(context.Context) ([]*Album, error)
+	Update(context.Context, uuid.UUID, *Album) error
+	Delete(context.Context, uuid.UUID) error
+}
+
+type service struct {
+	aR Repository
+}
+
+// NewService creates a service with the necessary dependencies.
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+// AddAlbum adds the given album to the database.
+func (s *service) AddAlbum(ctx context.Context, album *Album) error {
+	if err := s.aR.Add(ctx, album); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteAlbum deletes the given album from the database.
+func (s *service) DeleteAlbum(ctx context.Context, uuid uuid.UUID) error {
+	if err := s.aR.Delete(ctx, uuid); err != nil {
+		return err
+	}
+	return nil
+}
