98af73930916844cd090c011bb8ad2dd5c5aa3c5
Cyro Dubeux <xorycx@gmail.com>
2022-06-16 10:23:17 -0300

    Sprinft removed from middleware

--- shared/infra/middleware/middleware.go
+++ shared/infra/middleware/middleware.go
@@ -1,21 +1,25 @@
 package middleware
 
 import (
-	"fmt"
 	"hexagony/lib/clog"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		var sb strings.Builder
 
-		clog.Info(
-			fmt.Sprintf(
-				"method: %s, url: %s, agent: %s, referer: %s, proto: %s, remote_address: %s, latency: %s",
-				r.Method, r.URL.String(), r.UserAgent(), r.Referer(), r.Proto, r.RemoteAddr, time.Since(start),
-			))
+		sb.WriteString(
+			"method: " + r.Method + ", " + "url: " + r.URL.String() + ", " +
+				"agent: " + r.UserAgent() + ", " + "referer: " + r.Referer() + ", " +
+				"proto: " + r.Proto + ", " + "remote_address: " + r.RemoteAddr + ", " +
+				"latency: " + time.Since(start).String(),
+		)
+
+		clog.Info(sb.String())
 
 		next.ServeHTTP(w, r)
 	})
