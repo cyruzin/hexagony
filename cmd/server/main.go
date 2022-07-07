5a970b13961a64f4a61f47e9741345c28dd1c9ce
Cyro Dubeux <xorycx@gmail.com>
2022-07-07 14:56:10 -0300

    postgres ssl connection string fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -74,7 +74,7 @@ func main() {
 	// }
 
 	databaseURL := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s",
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"),
 	)
